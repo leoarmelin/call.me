6cc7c00091566836af4cabf452ee764445d08bdb
Leonardo Armelin <leonardoarmelin@codeby.com.br>
2021-10-25 20:57:16 -0300

    feat(Input): add onChange & value props

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,17 @@
 import { Input as StyledInput } from "./styles";
 
-export const Input = () => {
-  return <StyledInput placeholder="Insira seu nome" />;
+interface IInputProps {
+  placeholder?: string;
+  value?: string;
+  onChange?: (value: string) => void;
+}
+
+export const Input = ({ onChange, placeholder, value }: IInputProps) => {
+  return (
+    <StyledInput
+      placeholder={placeholder}
+      value={value}
+      onChange={(e) => onChange?.(e.target.value)}
+    />
+  );
 };
