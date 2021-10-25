56921db3e9f115911a667b654240b4bfbdf36ae9
Leonardo Armelin <leonardoarmelin@codeby.com.br>
2021-10-25 20:54:49 -0300

    feat(app): add UserContext provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
-import App from "./pages/App";
+import { UserContextProvider } from "./contexts/UserContext";
+import Home from "./pages/Home";
 import { GlobalStyles } from "./styles/global";
 import { theme } from "./styles/theme";
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <App />
+      <UserContextProvider>
+        <GlobalStyles />
+        <Home />
+      </UserContextProvider>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
